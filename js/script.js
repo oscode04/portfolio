f69ce10a749306e1356c957b360fa15424f3e9bf
gaos <hellogaos@gmail.com>
2023-01-11 22:04:33 +0700

    tingal rapihin responsive dan tambah link aja, untuk tampilan web baru 90%, responsive baru sampe about me

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-let aboutMe = document.getElementById("sec2");
 let landingPage = document.getElementById("sec1");
+let aboutMe = document.getElementById("sec2");
+let myWork = document.getElementById("sec3");
+let mySkill = document.getElementById("sec4");
+let contactMe = document.getElementById("sec5");
 
 fetch("../pages-html/landing-page.html")
   .then((snap) => snap.text())
@@ -11,4 +14,22 @@ fetch("../pages-html/about-me.html")
   .then((snap) => snap.text())
   .then((result) => {
     aboutMe.innerHTML = result;
+  });
+
+fetch("../pages-html/my-work.html")
+  .then((ambil) => ambil.text())
+  .then((hasil) => {
+    myWork.innerHTML = hasil;
+  });
+
+fetch("../pages-html/my-skill.html")
+  .then((ambil) => ambil.text())
+  .then((hasil) => {
+    mySkill.innerHTML = hasil;
+  });
+
+fetch("../pages-html/contact-me.html")
+  .then((ambil) => ambil.text())
+  .then((hasil) => {
+    contactMe.innerHTML = hasil;
   });
